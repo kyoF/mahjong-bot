005deefe31f86c2b0751c4f3469628facbd7e207
kyoF <fujiki.kyosuke@gmail.com>
2024-03-03 22:01:24 +0900

    fix: formatter

--- python/main.py
+++ python/main.py
@@ -1,14 +1,26 @@
+import slackweb
 import random
+import os
+
 
 def main():
-    s = [f"{i}s" for i in range(1, 10)]
-    m = [f"{i}m" for i in range(1, 10)]
-    p = [f"{i}p" for i in range(1, 10)]
-    j = ["to", "na", "sy", "pe", "hk", "ht", "ty"]
+    slack_url = slackweb.Slack(os.getenv('INCOMING_WEBHOOK_URL'))
+    s = [f'{i}s' for i in range(1, 10)]
+    m = [f'{i}m' for i in range(1, 10)]
+    p = [f'{i}p' for i in range(1, 10)]
+    j = ['to', 'na', 'sy', 'pe', 'hk', 'ht', 'ty']
     mountain = s * 4 + m * 4 + p * 4 + j * 4
     haipai = random.sample(mountain, 13)
-    haipai.sort(key=lambda x: (x[1:].isdigit(), x[1], x[0] if x[0].isdigit() else float('inf')))
+    haipai.sort(
+        key=lambda x: (
+            x[1:].isdigit(), x[1], x[0] if x[0].isdigit() else float('inf')
+        )
+    )
+    slack_url.notify(
+        text=haipai,
+    )
     print(haipai)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
