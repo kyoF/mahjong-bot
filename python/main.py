1d787fd79589304c406071ed35948ea9978a20c5
kyoF <fujiki.kyosuke@gmail.com>
2024-03-04 20:17:49 +0900

    fix: slack用の文字列を編集

--- python/main.py
+++ python/main.py
@@ -8,37 +8,16 @@ def main():
     load_dotenv()
     incoming_webhook_url = os.getenv('INCOMING_WEBHOOK_URL')
     slack_client = slackweb.Slack(incoming_webhook_url)
-    s = [f':mahjong-sou{i}:' for i in range(1, 10)]
     m = [f':mahjong-man{i}:' for i in range(1, 10)]
     p = [f':mahjong-pin{i}:' for i in range(1, 10)]
-    j = [':mahjong-ton:', ':mahjong-nan:', ':mahjong-sha:', 'mahjong-pei',
-         'mahjong-haku', 'mahjong-hatsu', 'mahjong-chun']
+    s = [f':mahjong-sou{i}:' for i in range(1, 10)]
+    j = [':mahjong-ton:', ':mahjong-nan:', ':mahjong-sha:', ':mahjong-pei:',
+         ':mahjong-haku:', ':mahjong-hatsu:', ':mahjong-chun:']
     mountain = s * 4 + m * 4 + p * 4 + j * 4
     haipai = random.sample(mountain, 13)
-    haipai.sort(
-        key=lambda x: (
-            x[1:].isdigit(), x[1], x[0] if x[0].isdigit() else float('inf')
-        )
-    )
-
-    slack_text = []
-    slack_text.append(
-        {
-            'blocks': [
-                {
-                    'type': 'section',
-                    'text': {
-                            'type': 'mrkdwn',
-                            'text': 'aaa'
-                    }
-                }
-            ]
-        }
-    )
 
     slack_client.notify(
-        text="今日の配牌:mahjong:",
-        attachments=slack_text
+        text=f"今日の配牌:mahjong:\n{haipai}",
     )
     print(haipai)
 
