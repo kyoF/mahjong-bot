74b93e2e9dbd9a6d4cc3670a12808fa4a80c3729
kyoF <fujiki.kyosuke@gmail.com>
2024-03-04 19:59:40 +0900

    add: notify slack

--- python/main.py
+++ python/main.py
@@ -1,14 +1,18 @@
+from dotenv import load_dotenv
 import slackweb
 import random
 import os
 
 
 def main():
-    slack_url = slackweb.Slack(os.getenv('INCOMING_WEBHOOK_URL'))
-    s = [f'{i}s' for i in range(1, 10)]
-    m = [f'{i}m' for i in range(1, 10)]
-    p = [f'{i}p' for i in range(1, 10)]
-    j = ['to', 'na', 'sy', 'pe', 'hk', 'ht', 'ty']
+    load_dotenv()
+    incoming_webhook_url = os.getenv('INCOMING_WEBHOOK_URL')
+    slack_client = slackweb.Slack(incoming_webhook_url)
+    s = [f':mahjong-sou{i}:' for i in range(1, 10)]
+    m = [f':mahjong-man{i}:' for i in range(1, 10)]
+    p = [f':mahjong-pin{i}:' for i in range(1, 10)]
+    j = [':mahjong-ton:', ':mahjong-nan:', ':mahjong-sha:', 'mahjong-pei',
+         'mahjong-haku', 'mahjong-hatsu', 'mahjong-chun']
     mountain = s * 4 + m * 4 + p * 4 + j * 4
     haipai = random.sample(mountain, 13)
     haipai.sort(
@@ -16,8 +20,25 @@ def main():
             x[1:].isdigit(), x[1], x[0] if x[0].isdigit() else float('inf')
         )
     )
-    slack_url.notify(
-        text=haipai,
+
+    slack_text = []
+    slack_text.append(
+        {
+            'blocks': [
+                {
+                    'type': 'section',
+                    'text': {
+                            'type': 'mrkdwn',
+                            'text': 'aaa'
+                    }
+                }
+            ]
+        }
+    )
+
+    slack_client.notify(
+        text="今日の配牌:mahjong:",
+        attachments=slack_text
     )
     print(haipai)
 
